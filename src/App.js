e8424ec4ee26af49d3da1253dbef5f95bcd0a9d6
Somsubhra Das <das.somsubhra1@gmail.com>
2020-04-14 01:56:44 +0530

    Created project structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+import styles from "./App.module.css";
+import { Cards, CountryPicker, Chart } from "./components";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className={styles.container}>
+                <h1>App</h1>
+                <Cards />
+                <CountryPicker />
+                <Chart />
+            </div>
+        );
+    }
 }
---
+++ src/components/CountryPicker/CountryPicker.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CountryPicker() {
+    return <h1>CountryPicker</h1>;
+}

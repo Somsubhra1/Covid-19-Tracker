98a06fcacfa573a9c479483a8b19fec9240bfa80
Somsubhra Das <das.somsubhra1@gmail.com>
2020-04-21 16:17:03 +0530

    Updates

---
+++ precache-manifest.073e4d32ad65098913f5435405d80eb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303091b8937e87a236d6a331e0188a6b",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "b53178009a6dae0c701c",
+    "url": "/Covid-19-Tracker/static/css/main.dd47be67.chunk.css"
+  },
+  {
+    "revision": "d51b272ce713e56a75c9",
+    "url": "/Covid-19-Tracker/static/js/2.abea9955.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Tracker/static/js/2.abea9955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53178009a6dae0c701c",
+    "url": "/Covid-19-Tracker/static/js/main.7c6f3bd1.chunk.js"
+  },
+  {
+    "revision": "db2c14f84c828093e1e0",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.a6448153.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19-Tracker/static/media/image.d7265326.png"
+  }
+]);

f5569262c3231590b18592e0090964a17ba85635
Somsubhra Das <das.somsubhra1@gmail.com>
2020-04-21 13:49:30 +0530

    Line chart added

--- src/api/index.js
+++ src/api/index.js
@@ -17,3 +17,16 @@ export const fetchData = async () => {
         return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {}
 };
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+        // console.log(data);
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+        return modifiedData;
+    } catch (error) {}
+};
